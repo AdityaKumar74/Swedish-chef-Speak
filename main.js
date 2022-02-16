ca5511e5b4e88c4a988443baf233f4e06491a12d
Aditya Kumar <adityakumar9939@gmail.com>
2022-02-16 23:00:40 +0530

    done with JS

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 var btnTranslate = document.getElementById("btn-translate");
 var txtArea = document.getElementById("txt-area");
-var outputDiv = document.getElementById("output");
+var outputDiv = document.getElementById("output")
+
+var serverURL = "https://api.funtranslations.com/translate/chef.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something went wrong! Try again later")
+}
+
 
 function clickHandler() {
-    outputDiv.innerText = txtArea.value;
-}
+    var inputText = txtArea.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
+
+};
 
 btnTranslate.addEventListener("click", clickHandler)
